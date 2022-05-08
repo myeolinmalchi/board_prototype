55f4c1c6d6e8d2e4786dc31640693d7d983807cb
myeolinmalchi <rkd2274@pusan.ac.kr>
2022-05-08 19:14:51 +0900

    Add: Data Transfer Object

---
+++ app/dto/AdminDTO.scala
@@ -0,0 +1,23 @@
+package dto
+
+import models.Tables.AdminRow
+import play.api.libs.json.Json
+
+case class AdminDTO(id: String,
+					pw: String,
+					name: Option[String],
+					email: Option[String],
+					phone: Option[String])
+
+object AdminDTO {
+	implicit val adminWrites = Json.writes[AdminDTO]
+	implicit val adminReads = Json.reads[AdminDTO]
+	implicit val rowToDto = (row: AdminRow) =>
+		AdminDTO(
+			id = row.id,
+			pw = row.password,
+			name = row.name,
+			email = row.email,
+			phone = row.phone
+		)
+}
---
+++ app/dto/PostDTO.scala
@@ -0,0 +1,65 @@
+package dto
+
+import models.Tables.{PostImagesRow, Posts}
+import play.api.libs.json.Json
+
+case class PostDTO(postId: Int,
+				   boardId: Int,
+				   title: String,
+				   thumbnail: String,
+				   content: String,
+				   sequence: Int,
+				   images: List[PostImageDTO] = Nil) {
+	
+	def setImages(images: List[PostImageDTO]): PostDTO =
+		PostDTO(postId, boardId, title, thumbnail, content, sequence, images)
+		
+}
+
+case class PostImageDTO(postId: Int,
+						postImageId: Int,
+						image: String,
+						sequence: Int)
+
+case class PostRequestDTO(postId: Option[Int],
+						  boardId: Int,
+						  title: String,
+						  thumbnail: String,
+						  content: String,
+						  images: List[String])
+
+case class ThumbnailDTO(postId: Int,
+						boardId: Int,
+						thumbnail: String)
+
+object PostImageDTO {
+	implicit val imageWrites = Json.writes[PostImageDTO]
+	implicit val imageReads = Json.reads[PostImageDTO]
+	implicit val rowToDto = (row: PostImagesRow) =>
+		PostImageDTO(row.postId, row.postId, row.image, row.sequence)
+	
+}
+object PostDTO {
+	implicit val postWrites = Json.writes[PostDTO]
+	implicit val postReads = Json.reads[PostDTO]
+	implicit val rowToDto = (row: Posts#TableElementType) =>
+			PostDTO(
+				row.postId,
+				row.boardId,
+				row.title,
+				row.thumbnail,
+				row.content,
+				row.sequence
+			)
+}
+object PostRequestDTO {
+	implicit val postRequestWrites = Json.writes[PostRequestDTO]
+	implicit val postRequestReads = Json.reads[PostRequestDTO]
+}
+object ThumbnailDTO {
+	implicit val thumbnailWrites = Json.writes[ThumbnailDTO]
+	implicit val thumbnailReads = Json.reads[ThumbnailDTO]
+	implicit val rowToDto = (row: Posts#TableElementType) =>
+			ThumbnailDTO(row.postId, row.boardId, row.thumbnail)
+}
+
